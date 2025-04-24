3a3c7a7465181565306dada7fd3e7bb02ba094f8
musabcs <mosab.anter555@gmail.com>
2025-04-24 21:23:54 +0300

    giriş yapma

---
+++ project/src/firebaseConfig.ts
@@ -0,0 +1,15 @@
+// filepath: project/src/firebaseConfig.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpU9R1Rg662_29X0nuUYBYylNWCmmYdYY",
+  authDomain: "aniverse5.firebaseapp.com",
+  projectId: "aniverse5",
+  storageBucket: "aniverse5.firebasestorage.app",
+  messagingSenderId: "486938827338",
+  appId: "1:486938827338:web:4fe1d13775a1eafe28a4b3",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
