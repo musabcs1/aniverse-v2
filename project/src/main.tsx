3a2944698a4ec197b63a33af5672a2a01924077b
musabcs <mosab.anter555@gmail.com>
2025-04-25 09:14:59 +0300

    bak

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { UserProvider } from './context/UserContext';
+import { UserProvider } from './contexts/UserContext';
 import App from './App';
-import { UserContext } from './context/UserContext';
+import { UserContext } from './contexts/UserContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
