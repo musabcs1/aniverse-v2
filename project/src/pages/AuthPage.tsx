c706af04e69a1c4bd3cad9089b6180a6bd1c4d0a
musabcs <mosab.anter555@gmail.com>
2025-04-25 10:23:43 +0300

    bilmem

--- project/src/pages/AuthPage.tsx
+++ project/src/pages/AuthPage.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from 'react';
-import { useUserContext } from '../contexts/UserContext';
+import { UserContext } from '../contexts/UserContext'; // UserContext'i import et
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebaseConfig';
 import { useNavigate } from 'react-router-dom';
