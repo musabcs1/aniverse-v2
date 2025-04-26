8e3779eb504786a5eef43f2bbbf7049586f725d9
musabcs <mosab.anter555@gmail.com>
2025-04-26 22:34:46 +0300

    bildirimler

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -1,23 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
-import NotificationsPage from './pages/NotificationsPage';
 import './index.css';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/notifications',
-    element: <NotificationsPage />,
-  },
-]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
